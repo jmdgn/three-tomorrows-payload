13d83532f562d2f26edd0b92b9383ebe4d877a8b
James_Mills <james.mills@live.com.au>
2025-04-25 09:59:31 +1000

    Ready for Render.com deployment

--- next.config.js
+++ next.config.js
@@ -1,9 +1,7 @@
 import { withPayload } from '@payloadcms/next/withPayload'
 import redirects from './redirects.js'
 
-const NEXT_PUBLIC_SERVER_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
-  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
-  : process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
+const NEXT_PUBLIC_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || `https://${process.env.HOST || 'localhost'}`
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -43,8 +41,16 @@ const nextConfig = {
     PORT: process.env.PORT || '3000',
     PAYLOAD_SECRET: process.env.PAYLOAD_SECRET,
     DATABASE_URI: process.env.DATABASE_URI,
-    MONGODB_URI: process.env.MONGODB_URI || process.env.DATABASE_URI,
+    MONGODB_URI: process.env.MONGODB_URI || 'mongodb+srv://jamesmills:AGKAt5Jis97CPwGY@cluster0.uhcdpi9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
     NEXT_PUBLIC_SERVER_URL
+  },
+  webpack: (config, { isServer }) => {
+    // If we are on the server, use the correct host
+    if (isServer) {
+      config.resolve.alias['@payloadcms/next/withPayload'] = require.resolve('@payloadcms/next/withPayload')
+    }
+
+    return config
   },
 }
 
