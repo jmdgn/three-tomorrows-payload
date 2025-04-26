c04ff3611c9e7f193d84319b8c4eada96a5de1ee
James_Mills <james.mills@live.com.au>
2025-04-27 07:48:44 +1000

    Ready for Render.com deployment

--- next.config.js
+++ next.config.js
@@ -5,28 +5,13 @@ const NEXT_PUBLIC_SERVER_URL = process.env.NEXT_PUBLIC_SERVER_URL || 'http://loc
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: ['localhost', 'threetomorrows.co', 'three-tomorrows-payload.vercel.app'],
-    remotePatterns: [
-      ...[NEXT_PUBLIC_SERVER_URL]
-        .filter(Boolean)
-        .map((item) => {
-          try {
-            const url = new URL(item)
-            if (!url.hostname) {
-              console.warn(`Missing hostname in URL: ${item}`)
-              return null
-            }
-            return {
-              hostname: url.hostname,
-              protocol: url.protocol.replace(':', ''),
-            }
-          } catch (error) {
-            console.warn(`Invalid URL in NEXT_PUBLIC_SERVER_URL: ${item}`)
-            return null
-          }
-        })
-        .filter(Boolean),
+    domains: [
+      'localhost',
+      'threetomorrows.co',
+      'three-tomorrows-payload.up.railway.app',
+      'three-tomorrows-payload.vercel.app',
     ],
+    remotePatterns: [],
   },
   reactStrictMode: true,
   redirects,
@@ -36,7 +21,6 @@ const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-
   env: {
     PORT: process.env.PORT || '3000',
     PAYLOAD_SECRET: process.env.PAYLOAD_SECRET,
@@ -44,11 +28,9 @@ const nextConfig = {
     MONGODB_URI: process.env.MONGODB_URI || process.env.DATABASE_URI,
     NEXT_PUBLIC_SERVER_URL,
   },
-
   experimental: {
     esmExternals: true,
   },
-
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback = {
@@ -59,7 +41,6 @@ const nextConfig = {
       }
     }
 
-    // Add topLevelAwait support
     config.experiments = {
       ...config.experiments,
       topLevelAwait: true,
@@ -67,8 +48,6 @@ const nextConfig = {
 
     return config
   },
-
-  // Help prevent "clientModules" errors in Vercel deployment
   output: 'standalone',
 }
 
