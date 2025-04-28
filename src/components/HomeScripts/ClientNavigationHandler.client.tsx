de920e29258ac10796dc8aaf6912d4c0be334514
James_Mills <james.mills@live.com.au>
2025-04-28 12:49:29 +1000

    Ready for Render.com deployment

--- src/components/HomeScripts/ClientNavigationHandler.client.tsx
+++ src/components/HomeScripts/ClientNavigationHandler.client.tsx
@@ -25,30 +25,5 @@ export default function ClientNavigationHandler() {
     return () => {}
   }, [])
 
-  useEffect(() => {
-    const isIOS = /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream
-    const isSafari = /Safari/.test(navigator.userAgent) && !/Chrome/.test(navigator.userAgent)
-
-    if (isIOS && isSafari) {
-      console.log('Applying iOS Safari URL bar fix - keeping URL bar fixed')
-
-      setTimeout(() => {
-        window.scrollTo(0, 1)
-      }, 100)
-
-      const handleScroll = () => {
-        if (window.scrollY < 5) {
-          window.scrollTo(0, 5)
-        }
-      }
-
-      window.addEventListener('scroll', handleScroll)
-
-      return () => {
-        window.removeEventListener('scroll', handleScroll)
-      }
-    }
-  }, [])
-
   return <HomePageNavigationHandler />
 }
